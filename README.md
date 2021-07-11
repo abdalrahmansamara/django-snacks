216a131dd92650060e45a4d36ed6f6baabaa69e0
abdalrahmansamara <abdsamara_1998@yahoo.com>
2021-07-11 14:30:23 +0300

    lab finished

---
+++ README.md
@@ -0,0 +1,3 @@
+# Python snacks
+
+this repository is a generic test, in order to practice a bit on Django library.
---
+++ snacks/tests.py
@@ -0,0 +1,15 @@
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse
+# Create your tests here.
+
+class test_status_code(SimpleTestCase):
+    def test_all_status_codes(self):
+        # url1 = reverse('home')
+        # url2 = reverse('about')
+        self.assertEqual(self.client.get(reverse('home')).status_code, 200)
+        self.assertEqual(self.client.get(reverse('about')).status_code, 200)
+
+    def test_all_template_use(self):
+        self.assertEqual(self.client.get(reverse('home')).templates[0].name, "home.html")
+        self.assertEqual(self.client.get(reverse('about')).templates[0].name, "about.html")
+        self.assertEqual(self.client.get(reverse('about')).templates[1].name, "base.html")
